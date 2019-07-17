8ddf8b5cc2e6a603f8fe8f47c396f6e49dbd93fc
cuncis1st <cuncis1st@gmail.com>
2019-07-17 21:59:57 +0700

    Initial commit

---
+++ app/src/main/java/com/example/sendtextwaexample/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.sendtextwaexample;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_send).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWhatsApp();
+            }
+        });
+    }
+
+    private void openWhatsApp() {
+        String smsNumber = "6281357570064";     // awal +62 berubah cuma 62 aja
+        try {
+            Intent sendIntent = new Intent("android.intent.action.MAIN");
+            sendIntent.setAction(Intent.ACTION_SEND);
+            sendIntent.setType("text/plain");
+            sendIntent.putExtra(Intent.EXTRA_TEXT, "Aku Orang Ganteng dan Intelek");
+            sendIntent.putExtra("jid", smsNumber + "@s.whatsapp.net");
+            sendIntent.setPackage("com.whatsapp");
+            startActivity(sendIntent);
+        } catch(Exception e) {
+            Toast.makeText(this, "Error/n" + e.toString(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
